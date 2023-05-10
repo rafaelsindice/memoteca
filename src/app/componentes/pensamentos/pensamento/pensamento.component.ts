981db977b8bcdccb3012fcf11ee5060e991d997c
rafaelsindice <rafaelmagnum90@hotmail.com>
2023-05-10 16:36:37 -0300

    feat classes acima de 256 char

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -6,14 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pensamento.component.css']
 })
 export class PensamentoComponent implements OnInit {
-  pensamento={
+  @Input() pensamento={
     conteudo: 'I love Angular',
     autoria: 'Jão',
-    modelo:'Modelo3'
+    modelo:'modelo3'
 
   }
  constructor(){ }
  ngOnInit():void {
 
+ }
+ larguraPensamento(): string{
+  if(this.pensamento.conteudo.length>=256){
+    return 'pensamento-g'
+  }
+  else{
+    return 'pensamento-p'
+  }
  }
 }
